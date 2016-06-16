3fd23ef0a206b59928a7c671bf9bdb3da7075689
Dan Wasyluk <dwasyluk@gmail.com>
2016-06-15 23:47:42 -0400

    Bundle js

--- compiled/SignMessageRequest.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="api.d.ts" />
-var API;
-(function (API) {
-    var Client;
-    (function (Client) {
-        'use strict';
-    })(Client = API.Client || (API.Client = {}));
-})(API || (API = {}));
-//# sourceMappingURL=SignMessageRequest.js.map
