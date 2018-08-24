f8f3d9e1f0b8e1687276294f6b655ee9a1961d4d
evscott <eliotvscott@gmail.com>
2018-08-24 14:45:41 -0700

    Added syscoin-cli command 'getblockhash'

--- api/general.service.ts
+++ api/general.service.ts
@@ -33,6 +33,7 @@ import { GetAddressUTXOsRequest } from '../model/getAddressUTXOsRequest';
 import { GetBlockResponse } from '../model/getBlockResponse';
 import { GetBlockchainInfoResponse } from '../model/getBlockchainInfoResponse';
 import { GetChainTipsResponse } from '../model/getChainTipsResponse';
+import { GetMemPoolInfoResponse } from '../model/getMemPoolInfoResponse';
 import { GetNewAddressRequest } from '../model/getNewAddressRequest';
 import { GetSpentInfoResponse } from '../model/getSpentInfoResponse';
 import { GovernanceInfoResponse } from '../model/governanceInfoResponse';
@@ -1595,6 +1596,48 @@ export class GeneralService {
         ];
 
         return this.httpClient.get<Info>(`${this.basePath}/getinfo`,
+            {
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * 
+     * Returns details on the active state of the TX memory pool.
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public getmempoolinfo(observe?: 'body', reportProgress?: boolean): Observable<GetMemPoolInfoResponse>;
+    public getmempoolinfo(observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<GetMemPoolInfoResponse>>;
+    public getmempoolinfo(observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<GetMemPoolInfoResponse>>;
+    public getmempoolinfo(observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+        let headers = this.defaultHeaders;
+
+        // authentication (token) required
+        if (this.configuration.apiKeys["token"]) {
+            headers = headers.set('token', this.configuration.apiKeys["token"]);
+        }
+
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+            'application/json'
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json'
+        ];
+
+        return this.httpClient.get<GetMemPoolInfoResponse>(`${this.basePath}/getmempoolinfo`,
             {
                 withCredentials: this.configuration.withCredentials,
                 headers: headers,
